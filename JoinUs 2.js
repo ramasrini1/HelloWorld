ff1651871a7ff3cf40183f45698a9426fed283d5
Rama Srinivas <ramasrini1@gmail.com>
2021-07-13 10:34:45 -0700

    Update JoinUs 2.js
    my new change

--- JoinUs 2.js
+++ JoinUs 2.js
@@ -6,6 +6,7 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 import  './JoinUs.css';
+my new change
 
 function JoinUs() {
   let myVar = "cool";
